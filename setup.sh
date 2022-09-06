a8e73dd39909dd8fa89fc96d5361aa8ec6442e9b
Nate Browne <npcompletenate@protonmail.com>
2022-09-06 10:04:34 -0700

    removing function for `git_current_branch`

--- setup.sh
+++ setup.sh
@@ -6,18 +6,4 @@ git config --global alias.type "cat-file -t"
 git config --global alias.dump "cat-file -p"
 git config --global commit.gpgsign true
 alias merge='gh pr merge -sd'
-
-function git_current_branch {
-        local ref
-        ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null) 
-        local ret=$? 
-        if [[ $ret != 0 ]]
-        then
-                [[ $ret == 128 ]] && return
-                ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null)  || return
-        fi
-        echo ${ref#refs/heads/}
-}
-alias ggpush="git push origin $(git_current_branch)"
-alias ggpull="git pull origin $(git_current_branch)"
 alias gcam="git commit -am"
